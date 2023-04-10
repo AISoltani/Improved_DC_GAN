036999c81597a47abb717b0fd656a01263f9c724
Amir <amirsoltani_os@hotmail.com>
2023-04-10 18:55:12 +0330

    Add files via upload

---
+++ test.py
@@ -0,0 +1,4 @@
+from model import Deep_Conv_GAN
+
+''' Initializing the DC_GAN module with the necessary paths '''
+Deep_Conv_GAN.inference(Deep_Conv_GAN, set_weight_dir='model.pth' , set_gen_dir='result_img')
